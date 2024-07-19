e1af40079b61a058682de61d55c2cbc28dd14271
Bret Cameron <bret@yulife.com>
2024-07-19 17:23:37 +0100

    Add connect_db fn

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,50 @@
+use std::env;
+
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
+use mongodb::{
+    bson::doc,
+    options::{ClientOptions, ServerApi, ServerApiVersion},
+    sync::Client,
+};
 
 async fn greet() -> impl Responder {
     HttpResponse::Ok().body("Hello, world!")
 }
 
+async fn connect_db() -> mongodb::error::Result<()> {
+    let uri = env::var("MONGODB_URI").expect("MONGODB_URI must be set");
+
+    println!("{}", uri);
+
+    let mut client_options = ClientOptions::parse(uri).await?;
+
+    let server_api = ServerApi::builder().version(ServerApiVersion::V1).build();
+    client_options.server_api = Some(server_api);
+
+    let client = Client::with_options(client_options)?;
+    client
+        .database("admin")
+        .run_command(doc! { "ping": 1 })
+        .await?;
+
+    Ok(())
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    match connect_db().await {
+        Ok(_) => println!("Successfully connected to the database."),
+        Err(e) => println!("Failed to connect to the database: {:?}", e),
+    }
+
     println!("Starting web server...");
 
     let server = HttpServer::new(|| App::new().route("/", web::get().to(greet)))
-        .bind("0.0.0.0:5001")?
+        .bind(("0.0.0.0", 5001))
+        .unwrap()
         .run();
 
     println!("Server running at http://localhost:5001/");
